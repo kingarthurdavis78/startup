4b10065f9ae84ad2217040a428900781d4f5dbf3
Davis Hunter <108430642+kingarthurdavis78@users.noreply.github.com>
2023-05-23 23:44:32 -0600

    add files

---
+++ player.js
@@ -0,0 +1,10 @@
+class Player {
+    constructor(name, roomCode) {
+    this.name = name;
+    this.roomCode = roomCode;
+    this.response = '';
+    this.vote = '';
+    this.score = 0;
+    }
+}
+
